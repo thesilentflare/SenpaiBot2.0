45700ef42c90e795a5db69e8ac7689b38ce23893
Gavin L <16308886+thesilentflare@users.noreply.github.com>
2023-05-05 20:46:35 +0800

    Init birthday sqls
    waiting on @snoopy for db design

--- src/database_helper.py
+++ src/database_helper.py
@@ -1,8 +1,10 @@
 import database
 import sqlite3
+
+
 def initialize(server_name):
     global db
-    db = server_name + '.db'
+    db = server_name + ".db"
     conn = sqlite3.connect(db)
     database.initialize(conn)
     conn.close()
--- src/senpaibot.py
+++ src/senpaibot.py
@@ -11,40 +11,45 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
-DESCRIPTION = '''The senpai of the server.'''
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), description=DESCRIPTION)
+DESCRIPTION = """The senpai of the server."""
+bot = commands.Bot(
+    command_prefix="!", intents=discord.Intents.all(), description=DESCRIPTION
+)
+
 
 @bot.event
 async def on_ready():
-	print('Logged in as')
-	print(bot.user.name)
-	print(bot.user.id)
-	print('------')
-	await bot.change_presence(activity=discord.Game(name='Ara-ara~~❤ Kouhai-kun'))
-	database_helper.initialize(str(bot.guilds[0].id))
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("------")
+    await bot.change_presence(activity=discord.Game(name="Ara-ara~~❤ Kouhai-kun"))
+    database_helper.initialize(str(bot.guilds[0].id))
+
 
 # @bot.command()
 # async def leave():
 #     leave_all_voice_channels(bot)
 #     print("Left all voice channels")
 
-# test command
-@bot.command(name='99')
-async def nine_nine(ctx):
-	brooklyn_99_quotes = [
-		'I\'m the human form of the 💯 emoji.',
-		'Bingpot!',
-		(
-			'Cool. Cool cool cool cool cool cool cool, '
-			'no doubt no doubt no doubt no doubt.'
-		),
-	]
 
-	response = random.choice(brooklyn_99_quotes)
-	await ctx.send(response)
+# test command
+@bot.command(name="99")
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        "I'm the human form of the 💯 emoji.",
+        "Bingpot!",
+        (
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt."
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
 
 # def signal_handler(signal, frame):
@@ -61,41 +66,44 @@ async def nine_nine(ctx):
 # 	# exit program
 # 	sys.exit(0)
 
+
 def leave_all_voice_channels(bot):
-	'''(Client) -> null
-	makes the Client leave all connected voice channels
-	'''
+    """(Client) -> null
+    makes the Client leave all connected voice channels
+    """
 
-	connected_voices = []
-	# get a list of all voice channels the bot is connected to
-	for voice in bot.voice_clients:
-		# if bot is connected to the channel, add it to the list
-		if (voice.is_connected()):
-			connected_voices.append(voice)
-	# if bot is not connected to any voice channel, print error message
-	if (connected_voices):
-		# disconnect bot from all connected voice channels
-		for voice in connected_voices:
-			voice.disconnect()
+    connected_voices = []
+    # get a list of all voice channels the bot is connected to
+    for voice in bot.voice_clients:
+        # if bot is connected to the channel, add it to the list
+        if voice.is_connected():
+            connected_voices.append(voice)
+    # if bot is not connected to any voice channel, print error message
+    if connected_voices:
+        # disconnect bot from all connected voice channels
+        for voice in connected_voices:
+            voice.disconnect()
 
-if (__name__ == "__main__"):
-	if (TOKEN is None):
-		print("Error: no token given")
-		sys.exit(1)
-	# for module in modules:
-	#       bot.load_extension(module)
-	loop = asyncio.new_event_loop()
-	asyncio.set_event_loop(loop)
-	try:
-		loop.run_until_complete(bot.start(TOKEN))
-		# asyncio.new_event_loop().run_until_complete(bot.start(TOKEN))
-	except KeyboardInterrupt:
-		print("INTERRUPTED...")
-		# asyncio.get_event_loop().run_until_complete(tally_before_exit())
-		loop.run_until_complete(bot.close())
-		# cancel all tasks lingering
 
-	finally:
-		loop.run_until_complete(bot.close())
-		# unsure to keep below or not
-		# loop.run_until_complete(asyncio.gather(*asyncio.all_tasks()))
+if __name__ == "__main__":
+    if TOKEN is None:
+        print("Error: no token given")
+        sys.exit(1)
+    # TODO: modularize
+    # for module in modules:
+    #       bot.load_extension(module)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(bot.start(TOKEN))
+        # asyncio.new_event_loop().run_until_complete(bot.start(TOKEN))
+    except KeyboardInterrupt:
+        print("INTERRUPTED...")
+        # asyncio.get_event_loop().run_until_complete(tally_before_exit())
+        loop.run_until_complete(bot.close())
+        # cancel all tasks lingering
+
+    finally:
+        loop.run_until_complete(bot.close())
+        # TODO: unsure to keep below or not
+        # loop.run_until_complete(asyncio.gather(*asyncio.all_tasks()))

56928779af8762684d984b2c0d885f75aba95717
Gavin L <16308886+thesilentflare@users.noreply.github.com>
2023-05-24 00:55:00 +0800

    Added create birthday FE

--- models/urls.py
+++ models/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("logout/", views.logout_user, name="logout"),
+    path("birthdays/", views.birthdays, name="birthdays"),
+    # path("delete_birthday/<int:pk>", views.delete_birthday, name="delete_birthday"),
+    path("add_birthday/", views.add_birthday, name="add_birthday"),
+    # path("update_birthday/<int:pk>", views.update_birthday, name="update_birthday"),
 ]
--- models/views.py
+++ models/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Birthday
+from .forms import AddBirthdayForm
 
 # Create your views here.
 def home(request):
@@ -24,3 +26,37 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged Out")
     return redirect("home")
+  
+def birthdays(request):
+    birthdays = Birthday.objects.all()
+    
+    
+    # check to see if logging in
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # auth
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged In")
+            return redirect("birthdays")
+        else:
+            messages.error(request, "Error logging in")
+            return redirect("home")
+    else:
+        return render(request, "birthdays.html", {'birthdays': birthdays})
+      
+def add_birthday(request):
+    form = AddBirthdayForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_birthday = form.save()
+                messages.success(request, "Birthday Added")
+                return redirect('birthdays')
+        
+        return render(request, "add_birthday.html", {'form': form})
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')

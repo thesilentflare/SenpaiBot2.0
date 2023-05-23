b5ea8e0bd0a623052858a9dd07b2c6053bdfe182
Gavin L <16308886+thesilentflare@users.noreply.github.com>
2023-05-23 21:33:54 +0800

    Add basic custom frontend

---
+++ models/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("logout/", views.logout_user, name="logout"),
+]

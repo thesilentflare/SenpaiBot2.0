bdd2678e8a5d08a608b79c0a8b113f3ade3ce64f
Gavin L <16308886+thesilentflare@users.noreply.github.com>
2024-08-16 21:19:52 +0800

    wip: add primitive nh search

--- src/senpai_nh.py
+++ src/senpai_nh.py
@@ -20,6 +20,23 @@ async def send_embed_nh_msg(context, title, post_url, cover_url, doujin_id, douj
   embed_msg.set_image(url=cover_url)
   # send message
   await context.send(embed=embed_msg)
+  
+async def send_embed_nh_list(context, search_query, res_list):
+    embed_msg = discord.Embed(title=f"Searched: {search_query}", color=0xFF93AC)
+    # print("here")
+    for res in res_list[:10]:
+      # print(res)
+      title = res.title(Format.Pretty)
+      id = hash(res)
+      msg_name = f"[{id}: {title}]({res.url})"
+      
+      msg_value = f"Favs: {res.num_favorites} | Pages: {res.num_pages}"
+      
+      embed_msg.add_field(name=msg_name, value="`{}`".format(msg_value), inline=False)
+    
+    
+    # send message
+    await context.send(embed=embed_msg)
 
 def parse_tags(tags_list):
   formatted_tags = ""
@@ -48,6 +65,43 @@ class SenpaiNh(commands.Cog):
       
       await send_embed_nh_msg(context, doujin_title, random_doujin.url, doujin_cover, doujin_id, doujin_tags, doujin_num_pages, doujin_num_favs)
 
+
+  @commands.group(invoke_without_command=True)
+  async def nhs(self, context, *arg):
+    offset = len("!nhs")
+    question = context.message.content[offset + 1 :]
+    res = []
+    
+    if len(question) == 0:
+      await context.send(
+          "`Usage:\n"
+          + "!nhs tag [tag_name]\n"
+          + "!nhs key [keyword]\n`"
+      )
+      return
+    await context.send(
+          "`SEARCHING...`\n"
+          + "`Please wait a moment while I search for the doujins.`\n"
+      )
+    search_criteria = arg[1]
+    if arg[0] == "tag":
+      # get random doujin by tag
+      query = f"tag:{search_criteria}"
+      res = await listSearchBy(query)
+    elif arg[0] == "key":
+      res = await listSearchBy(search_criteria)
+    # print(res)
+    if len(res) == 0:
+      await context.send("No doujins found.")
+      return
+    await send_embed_nh_list(context, search_criteria, res)
+
+async def listSearchBy(query_search):
+  result_list = []
+  search_query_with_time = f"{query_search} uploaded:30d"
+  for doujin in Utils.search_all_by_query(query=search_query_with_time, sort=Sort.PopularWeek):
+    result_list.append(doujin)
+  return result_list
 
 async def setup(bot):
     await bot.add_cog(SenpaiNh(bot))
